052b2de663bbd39235c5fd071dce03716f53e569
Dr-Olami <muhammedabdulrasheed1992@gmail.com>
2023-09-22 04:18:11 -0700

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,65 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import python from "./tech/python.png";
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import wordpress from "./tech/wordpress.png";   
+import django from "./tech/django.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import flask from "./tech/flask.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+
+
+import oyorooms from "./company/oyorooms.png";
+import zeta from "./company/zeta.png";
+import ninjacart from "./company/ninjacart.png";
+import ather from "./company/ather.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  wordpress,
+  python,
+  css,
+  docker,
+  figma,
+  django,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  flask,
+  tailwind,
+  threejs,
+  oyorooms,
+  zeta,
+  ninjacart,
+  ather,
+  carrent,
+  jobit,
+  tripguide,
+};
